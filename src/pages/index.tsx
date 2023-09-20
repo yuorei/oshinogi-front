0eda2afec9299085ecb85d3c75d082f779a3faee
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-20 23:48:44 +0900

    政治家一覧追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from 'next/head'
-// import styles from '@/styles/Home.module.css'
+import Router from 'next/router'
+import styles from '@/styles/Home.module.css'
 import Header from '@/components/header/header'
+import Link from 'next/link';
+
+const politicianList = [
+  { id: 1, name: 'User 1', district: '東京都' },
+  { id: 2, name: 'User 2', district: '愛知県' },
+  { id: 3, name: 'User 3', district: '大阪府' },
+];
 
 
 export default function Home() {
+  const buttonAlert = () => {
+    Router.push('/politician/new')
+  }
   return (
     <>
       <Head>
@@ -12,8 +23,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <main >
-        <Header />
+        <div className={styles.select}>
+          <p className={styles.p}>推しを選んでください</p>
+          <button className={styles.button} onClick={buttonAlert}>新たに作る</button>
+        </div>
+        <ul>
+          {politicianList.map((politician) => (
+            <Link className={styles.list} href={`/politician/${politician.id}`}>
+              <p className={styles.politician}>名前: {politician.name}</p>
+              <p className={styles.politician}>地区: {politician.district}</p>
+            </Link>
+          ))}
+        </ul>
       </main>
     </>
   )
